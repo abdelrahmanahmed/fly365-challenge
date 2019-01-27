22b1e74a29df9c28d78e59319b9f4fbb4a8a3354
abdelrahman wahdan <abdelrahman.wahdan1@vodafone.com>
2019-01-27 12:49:38 +0200

    added reducers

--- src/constants/action-types.constants.js
+++ src/constants/action-types.constants.js
@@ -1,2 +1,3 @@
 export const FETCH_HOTELS = 'fetch_hotels';
 export const FETCH_HOTELS_DETAILS = 'fetch_hotels_details';
+export const UPDATE_NUMBER_OF_NIGHTS = 'update_number_of_nights';
--- src/reducers/hotels.reducer.js
+++ src/reducers/hotels.reducer.js
@@ -1,12 +1,25 @@
-import { FETCH_HOTELS, FETCH_HOTELS_DETAILS } from '../constants/action-types.constants';
+import { FETCH_HOTELS, FETCH_HOTELS_DETAILS, UPDATE_NUMBER_OF_NIGHTS } from '../constants/action-types.constants';
 
-export default function (state = {}, action) {
+const initalState = {
+    numberOfNights: 1
+}
+
+export default function (state = initalState, action) {
     switch (action.type) {
         case FETCH_HOTELS:
-            return action.payload;
+            return Object.assign({}, state, {
+                list: action.payload.data
+            });
 
         case FETCH_HOTELS_DETAILS:
-            return action.payload;
+            return Object.assign({}, state, {
+                details: action.payload.data
+            });
+
+        case UPDATE_NUMBER_OF_NIGHTS:
+            return Object.assign({}, state, {
+                numberOfNights: action.payload
+            });
 
         default:
             return state;
