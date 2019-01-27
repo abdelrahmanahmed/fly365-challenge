8f4b37f06ae02a23e2b259e35417510505710680
abdelrahman wahdan <abdelrahman.wahdan1@vodafone.com>
2019-01-27 12:48:29 +0200

    removed materail UI warning

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 /*************************************** Providers ***************************************/
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { Provider } from 'react-redux';
 /*************************************** Components ***************************************/
-import Home from './components/home';
+import Home from './components/home.component';
+/*************************************** Stores ***************************************/
+import store from './store/store';
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 ReactDOM.render(
-  <MuiThemeProvider>
-    <Home />
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider theme={theme}>
+      <Home />
+    </MuiThemeProvider>
+  </Provider>
   , document.querySelector('.container'));
